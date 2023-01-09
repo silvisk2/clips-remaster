8a02a9f425575fb5eb4fc6c4d9744fbac8274a6f
BITDEFENDER\scapraru <scapraru@bitdefender.com>
2023-01-09 23:37:49 +0200

    3 mores from this module

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -1,7 +1,9 @@
-import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection, DocumentReference, QuerySnapshot } from '@angular/fire/compat/firestore';
 import { Injectable } from '@angular/core';
 import IClip from '../models/clip.model';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { of } from 'rxjs';
+import { switchMap , map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +12,36 @@ export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>
 
   constructor(
-    private db: AngularFirestore
+    private db: AngularFirestore,
+    private auth: AngularFireAuth
   ) {
     this.clipsCollection = db.collection('clips')
   }
 
   createClip(data: IClip) : Promise<DocumentReference<IClip>> {
     return this.clipsCollection.add(data)
+  }
+
+  getUserClips() {
+    return this.auth.user.pipe(
+      switchMap(user => {
+        if(!user) {
+          return of([])
+        }
+
+        const query = this.clipsCollection.ref.where(
+          'uid', '==', user.uid
+        )
+        return query.get()
+      }),
+      map(snapshot => (snapshot as QuerySnapshot<IClip>).docs)
+    )
+  }
+
+  updateClip(id: string, title: string) {
+    return this.clipsCollection.doc(id).update({
+      title
+    })
+
   }
 }
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,8 @@
+import { ModalService } from './../../services/modal.service';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ClipService } from 'src/app/services/clip.service';
+import IClip from 'src/app/models/clip.model';
 
 @Component({
   selector: 'app-manage',
@@ -9,15 +12,29 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 export class ManageComponent implements OnInit {
   videoOrder = '1'
+  clips: IClip[] = []
+  activeClip: IClip | null = null
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private clipService: ClipService,
+    private modal: ModalService
   ) {}
 
   ngOnInit() {
     this.route.queryParamMap.subscribe((params: Params) => {
       this.videoOrder = params['sort'] === '2' ? params['sort'] : '1'
+    })
+    this.clipService.getUserClips().subscribe(docs => {
+      this.clips = []
+
+      docs.forEach(doc => {
+        this.clips.push({
+          docID: doc.id,
+          ...doc.data()
+        })
+      })
     })
   }
 
@@ -28,6 +45,22 @@ export class ManageComponent implements OnInit {
       relativeTo: this.route,
       queryParams: {
         sort: value
+      }
+    })
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault()
+
+    this.activeClip = clip
+
+    this.modal.toggleModal('editClip')
+  }
+
+  update($event: IClip) {
+    this.clips.forEach((element, index) => {
+      if(element.docID == $event.docID) {
+        this.clips[index].title = $event.title
       }
     })
   }
