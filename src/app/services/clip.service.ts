9e84112139d828f5397a1ca46ce35e57c3d3a2d8
BITDEFENDER\scapraru <scapraru@bitdefender.com>
2023-01-10 18:50:16 +0200

    finish uploadind files module

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -4,6 +4,8 @@ import IClip from '../models/clip.model';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { of } from 'rxjs';
 import { switchMap , map } from 'rxjs/operators'
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { BehaviorSubject, combineLatest } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,8 @@ export class ClipService {
 
   constructor(
     private db: AngularFirestore,
-    private auth: AngularFireAuth
+    private auth: AngularFireAuth,
+    private storage: AngularFireStorage
   ) {
     this.clipsCollection = db.collection('clips')
   }
@@ -22,15 +25,22 @@ export class ClipService {
     return this.clipsCollection.add(data)
   }
 
-  getUserClips() {
-    return this.auth.user.pipe(
-      switchMap(user => {
+  getUserClips(sort$: BehaviorSubject<string>) {
+    return combineLatest([
+      this.auth.user,
+      sort$
+    ]).pipe(
+      switchMap(values => {
+        const [user, sort] = values
+
         if(!user) {
           return of([])
         }
-
         const query = this.clipsCollection.ref.where(
           'uid', '==', user.uid
+        ).orderBy(
+          'timestamp',
+          sort === '1' ? 'desc' : 'asc'
         )
         return query.get()
       }),
@@ -42,6 +52,12 @@ export class ClipService {
     return this.clipsCollection.doc(id).update({
       title
     })
+  }
+  async deleteClip(clip: IClip) {
+    const clipRef = this.storage.ref(`clips/${clip.fileName}`)
 
+    await clipRef.delete()
+
+    await this.clipsCollection.doc(clip.docID).delete()
   }
 }
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
 import IClip from 'src/app/models/clip.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-manage',
@@ -14,19 +15,23 @@ export class ManageComponent implements OnInit {
   videoOrder = '1'
   clips: IClip[] = []
   activeClip: IClip | null = null
+  sort$: BehaviorSubject<string>
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private clipService: ClipService,
     private modal: ModalService
-  ) {}
+  ) {
+    this.sort$ = new BehaviorSubject(this.videoOrder)
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.route.queryParamMap.subscribe((params: Params) => {
       this.videoOrder = params['sort'] === '2' ? params['sort'] : '1'
+      this.sort$.next(this.videoOrder)
     })
-    this.clipService.getUserClips().subscribe(docs => {
+    this.clipService.getUserClips(this.sort$).subscribe(docs => {
       this.clips = []
 
       docs.forEach(doc => {
@@ -59,8 +64,19 @@ export class ManageComponent implements OnInit {
 
   update($event: IClip) {
     this.clips.forEach((element, index) => {
-      if(element.docID == $event.docID) {
+      if (element.docID == $event.docID) {
         this.clips[index].title = $event.title
+      }
+    })
+  }
+  deleteClip($event: Event, clip: IClip) {
+    $event.preventDefault()
+
+    this.clipService.deleteClip(clip)
+
+    this.clips.forEach((element, index) => {
+      if (element.docID == clip.docID) {
+        this.clips.splice(index, 1)
       }
     })
   }
