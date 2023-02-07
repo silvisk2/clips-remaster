024375b893573ecbfd1e9a4df6c8604f6670861e
Silviu CAPRARU <scapraru@bitdefender.com>
2023-02-08 00:35:03 +0200

    2 and finish

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,28 +1,32 @@
 import  videojs  from 'video.js';
-
-
+import  IClip from 'src/app/models/clip.model';
 import { ActivatedRoute, Params } from '@angular/router';
 import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.css'],
   encapsulation: ViewEncapsulation.None,
+  providers: [DatePipe]
 })
 export class ClipComponent implements OnInit {
-  id = ''
   @ViewChild('videoPlayer', { static: true }) target?: ElementRef
   player?: videojs.Player
-
+  clip?: IClip
 
   constructor(public route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.player = videojs(this.target?.nativeElement)
 
-    this.route.params.subscribe((params: Params) => {
-      this.id = params['id'];
+    this.route.data.subscribe(data => {
+      this.clip = data['clip'] as IClip
+
+      this.player?.src({
+        src: this.clip.url,
+        type: 'video/mp4'
+      })
     })
   }
 }
